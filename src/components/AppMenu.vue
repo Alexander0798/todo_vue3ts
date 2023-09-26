d33aa7be0b7ee4fb1c8875d4c931e47573c94c5f
Alexander0798 <rav89515139989@yandex.ru>
2023-09-27 01:36:08 +0300

    Добавил Компонент кнопки и компонет меню

---
+++ src/components/AppMenu.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="menu">
+        <AppButton class="menu__button-add">Добавить задачу</AppButton>
+        <div class="menu__container">
+            <AppButton class="menu__button-login--up">Зарегистрироваться</AppButton>
+            <AppButton class="menu__button-login--in">Войти</AppButton>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+export default {}
+</script>
+<style lang="scss">
+.menu {
+    display: flex;
+    justify-content: space-between;
+&__button-add {
+}
+
+&__container {
+    display: flex;
+    gap: 0 10px
+}
+}
+
+
+
+</style>
---
+++ src/components/UI/AppButton.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "AppButton"
+})
+
+
+</script>
+<style scoped>
+.button {
+  min-width: 40px;
+  min-height: 20px;
+  border: 1px solid rebeccapurple;
+  border-radius: 20px;
+  padding: 5px 10px;
+}
+</style>
